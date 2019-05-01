25a0ca9787c4de857b3dbff5e59a265ffc788981
Saeed Rahman <saeedrahman@cdnis.edu.hk>
2019-05-02 07:34:52 +0800

    Initial Commit

---
+++ Grade8AppTemplate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Grade8AppTemplate
+//
+//  Created by Saeed Rahman on 2/5/2019.
+//  Copyright © 2019 Saeed Rahman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
