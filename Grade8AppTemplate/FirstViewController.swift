f35ff246a4cddea3082d3cc8dfb79cdff45afb78
Saeed Rahman <saeedrahman@users.noreply.github.com>
2021-11-03 11:15:59 +0800

    Removed Copyright Notice
    - edited the redundant copyright notices from each of the class files, and replaced with guidelines

--- Grade8AppTemplate/FirstViewController.swift
+++ Grade8AppTemplate/FirstViewController.swift
@@ -1,8 +1,25 @@
-//
-//  ViewController.swift
-//  Grade8AppTemplate
-//
-//
+/*
+ 
+ FirstViewController.swift
+ 
+ This file will contain the code for the first viewcontroller.
+ Please ensure that your code is organised and is easy to read.
+ This means that you will need to both structure your code correctly,
+ in addition to using the correct syntax for Swift.
+ 
+ Unless you are told otherwise, ensure that you are using the
+ camelCase syntax. For example, outputLabel and firstName are good
+ examples of using the camelCase syntax.
+ 
+ All @IBOutlets should be listed at the top of the class file, then
+ all variables, and then all functions (including @IBActions which are
+ essentially functions)
+ 
+ Please include details comments on your code to both demonstrate
+ understanding, and which serve you as a reminder as to what your code
+ actually does.
+ 
+ */
 
 import UIKit
 
